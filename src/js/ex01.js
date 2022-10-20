ac731e9a0ac7b5a1c68ece9e7832c69591902787
wisheee <wishee.u@gmail.com>
2022-10-20 15:31:08 +0900

    Setting the axis and grid helpers

--- src/js/ex01.js
+++ src/js/ex01.js
@@ -1,24 +1,21 @@
 import * as THREE from 'three';
 
-// ----- 주제: 기본 장면
+// ----- 주제: AxesHelper, GridHelper
 
 export default function example() {
-  // 동적으로 캔버스 조립하기
-  // const renderer = new THREE.WebGLRenderer();
-  // renderer.setSize(window.innerWidth, window.innerHeight);
-  // document.body.appendChild(renderer.domElement);
-
+  // Renderer
   const canvas = document.querySelector('#three-canvas');
   const renderer = new THREE.WebGLRenderer({
     canvas,
     antialias: true,
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
   // Scene
   const scene = new THREE.Scene();
 
-  // Perspective Camera(원근 카메라)
+  // Camera
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -26,36 +23,59 @@ export default function example() {
     1000
   );
   camera.position.x = 1;
-  camera.position.y = 2;
-  camera.position.z = 5;
+  camera.position.y = 3;
+  camera.position.z = 0;
   scene.add(camera);
 
-  // Orthographic Camera(직교 카메라)
-  // const camera = new THREE.OrthographicCamera(
-  //   -(window.innerWidth / window.innerHeight),
-  //   window.innerWidth / window.innerHeight,
-  //   1,
-  //   -1,
-  //   0.1,
-  //   1000
-  // );
-  // camera.position.x = 1;
-  // camera.position.y = 2;
-  // camera.position.z = 5;
-  // camera.lookAt(0, 0, 0);
-  // camera.zoom = 0.5;
-  // camera.updateProjectionMatrix();
-  // scene.add(camera);
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // AxesHelper
+  const axesHelper = new THREE.AxesHelper(3);
+  scene.add(axesHelper);
+
+  // GridHelper
+  const gridHelper = new THREE.GridHelper(5);
+  scene.add(gridHelper);
 
   // Mesh
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({
-    // color: 0xff0000
-    color: '#ff0000',
+  const material = new THREE.MeshStandardMaterial({
+    color: 'seagreen',
   });
   const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 2;
   scene.add(mesh);
 
+  camera.lookAt(mesh.position);
+
   // 그리기
-  renderer.render(scene, camera);
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const time = clock.getElapsedTime();
+
+    mesh.rotation.y = time;
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
 }
