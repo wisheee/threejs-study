e7020e62fb054ef79a89112619207ae3b69dbaa3
wisheee <wishee.u@gmail.com>
2022-10-20 14:52:42 +0900

    Animation using library (gsap)

---
+++ src/js/ex08.js
@@ -0,0 +1,81 @@
+import * as THREE from 'three';
+import gsap from 'gsap';
+
+// ----- 주제: 라이브러리를 이용한 애니메이션
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  scene.fog = new THREE.Fog('black', 3, 7);
+
+  // Perspective Camera(원근 카메라)
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.y = 3;
+  light.position.z = 5;
+  scene.add(light);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    // color: 0xff0000
+    color: '#ff0000',
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // 그리기
+  let oldTime = Date.now();
+
+  function draw() {
+    const newTime = Date.now();
+    const deltaTime = newTime - oldTime;
+    oldTime = newTime;
+
+    renderer.render(scene, camera);
+
+    // requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+
+  // gsap
+  gsap.to(mesh.position, {
+    duration: 1,
+    y: 2,
+    z: 3,
+  });
+
+  function setSize() {
+    // 카메라
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // updateProjectionMatrix 카메라 투영에 관련된 값에 변화가 있을 경우 실행해야 함
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
