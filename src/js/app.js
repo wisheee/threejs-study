e0c610dcaa3b957e5e3f31ed59be467680fcb301
wisheee <wishee.u@gmail.com>
2022-10-25 11:18:03 +0900

    MeshNormalMaterial

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import '../sass/main.scss';
 // import example from './ex07';
 // import example from './ex08';
 // import example from './ex09';
-import example from './ex10';
+// import example from './ex10';
+import example from './ex11';
 
 example();
