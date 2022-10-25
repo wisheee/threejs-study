68f628e2c2f481aadb60153e0ef19e3502c52a91
wisheee <wishee.u@gmail.com>
2022-10-25 11:12:01 +0900

    MeshToonMaterial

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@ import '../sass/main.scss';
 // import example from './ex06';
 // import example from './ex07';
 // import example from './ex08';
-import example from './ex09';
+// import example from './ex09';
+import example from './ex10';
 
 example();
