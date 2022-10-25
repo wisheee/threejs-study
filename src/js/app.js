c76924af0f03a6556cfb4b83d4d4e942da87cd16
wisheee <wishee.u@gmail.com>
2022-10-25 10:11:50 +0900

    Creating a cube with various textures applied

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ import '../sass/main.scss';
 // import example from './ex05';
 // import example from './ex06';
 // import example from './ex07';
-import example from './ex08';
+// import example from './ex08';
+import example from './ex09';
 
 example();
