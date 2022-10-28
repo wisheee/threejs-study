a08e2feb50724f8e9feaad1f7e5cd4cee3a5d571
wisheee <wishee.u@gmail.com>
2022-10-28 10:30:43 +0900

    init raycaster branch

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,4 @@
 import '../sass/main.scss';
-// import example from './ex01';
-// import example from './ex02';
-// import example from './ex03';
-// import example from './ex04';
-// import example from './ex05';
-import example from './ex06';
+import example from './ex01';
 
 example();
