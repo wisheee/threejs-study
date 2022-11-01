48c9e56044b60ede1fb9123858a952aa371f9139
wisheee <wishee.u@gmail.com>
2022-11-01 10:05:17 +0900

    Avoid drag click

--- src/js/ex02.js
+++ src/js/ex02.js
@@ -72,21 +72,24 @@ export default function example() {
   function draw() {
     const time = clock.getElapsedTime();
 
-    boxMesh.position.y = Math.sin(time) * 2;
-    torusMesh.position.y = Math.cos(time) * 2;
-    boxMesh.material.color.set('plum');
-    torusMesh.material.color.set('lime');
+    // boxMesh.position.y = Math.sin(time) * 2;
+    // torusMesh.position.y = Math.cos(time) * 2;
+    // boxMesh.material.color.set('plum');
+    // torusMesh.material.color.set('lime');
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);
   }
 
   function checkIntersects() {
+    if (mouseMoved) return;
+
     raycaster.setFromCamera(mouse, camera);
 
     const intersects = raycaster.intersectObjects(meshes);
     for (const item of intersects) {
       console.log(item.object.name);
+      item.object.material.color.set('red');
       break;
     }
     // console.log(intersects[0]);
@@ -101,11 +104,31 @@ export default function example() {
 
   // 이벤트
   window.addEventListener('resize', setSize);
-  canvas.addEventListener('click', e => {
-    mouse.x = e.clientX / canvas.clientWidth * 2 - 1;
-    mouse.y = -(e.clientY / canvas.clientHeight * 2 - 1);
+  canvas.addEventListener('click', (e) => {
+    mouse.x = (e.clientX / canvas.clientWidth) * 2 - 1;
+    mouse.y = -((e.clientY / canvas.clientHeight) * 2 - 1);
     // console.log(mouse);
     checkIntersects();
+  });
+  let mouseMoved; // 마우스를 드래그 했는지 true/false
+  let clickStartX;
+  let clickStartY;
+  let clickStartTime;
+  canvas.addEventListener('mousedown', (e) => {
+    clickStartX = e.clientX;
+    clickStartY = e.clientY;
+    clickStartTime = Date.now();
+  });
+  canvas.addEventListener('mouseup', (e) => {
+    const xGap = Math.abs(e.clientX - clickStartX);
+    const yGap = Math.abs(e.clientY - clickStartY);
+    const timeGap = Date.now() - clickStartTime;
+
+    if (xGap > 5 || yGap > 5 || timeGap > 500) {
+      mouseMoved = true;
+    } else {
+      mouseMoved = false;
+    }
   });
 
   draw();
